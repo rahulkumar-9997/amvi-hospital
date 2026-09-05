2f7f1105f2b3796ea0551d1361581e32304f7595
rahulkumar-9997 <rahulkumarmaurya464@gmail.com>
2026-09-05 15:46:46 +0530

    Refactor edit method in ProfileController for improved readability

--- app/Http/Controllers/Backend/ProfileController.php
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -16,8 +16,7 @@ class ProfileController extends Controller
 {
     public function edit(User $profile)
     {
-        $this->authorizeOwner($profile);
- 
+        $this->authorizeOwner($profile); 
         return view('backend.pages.profile.edit', ['user' => $profile]);
     }
  
